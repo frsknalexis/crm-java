c9927d0dfe8eb0c2dd8fb11beba603f852ca9e3a
Alexis Gutierrez <frsknalexis.1997@gmail.com>
2019-03-30 02:08:13 -0500

    Subiendo Proyecto Base

---
+++ crm_backend/src/main/java/com/dev/crm/CrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dev.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmBackendApplication.class, args);
+	}
+
+}
