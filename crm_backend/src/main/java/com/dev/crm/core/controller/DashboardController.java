aaeb9574f6ba7d58346190ca0bbd6314ca3df4a2
Alexis Gutierrez <frsknalexis.1997@gmail.com>
2019-03-31 03:48:28 -0500

    Configuracion del Template

---
+++ crm_backend/src/main/java/com/dev/crm/core/controller/DashboardController.java
@@ -0,0 +1,16 @@
+package com.dev.crm.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dev.crm.core.util.Constantes;
+
+@Controller
+public class DashboardController {
+
+	@GetMapping("/dashboard")
+	public String dashboardViewController() {
+		
+		return Constantes.DASHBOARD_VIEW;
+	}
+}
---
+++ crm_backend/src/main/java/com/dev/crm/core/util/Constantes.java
@@ -0,0 +1,7 @@
+package com.dev.crm.core.util;
+
+public class Constantes {
+
+	public static final String DASHBOARD_VIEW = "dashboard";
+	
+}
